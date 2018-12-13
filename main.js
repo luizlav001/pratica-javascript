fed6b6cd36d74431f505194e38f6399f5ff06ebc
luizlav001 <lincolndovale07@gmail.com>
2018-12-13 20:57:22 -0200

    Ajustes de estilo

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
 function preencherPerfil(info) {
   var template = `
-      <figure>
-            <img src="" alt="">
-          </figure>
-          <h1 class="nome">${info.name}</h1>
+    <h2>${info.name}</h2>
+    <ul>
+      <li>
+        <span>Altura:</span>
+        <span>${info.height}</span>
+      </li>
+      <li>
+        <span>Cor do cabelo:</span>
+        <span>${info.hair_color}</span>
+      </li>
+      <li>
+        <span>Ano de nascimento:</span>
+        <span>${info.birth_year}</span>
+      </li>
+      <li>
+        <span>Naves:</span>
+        <span id="naves">
           <ul>
-            <li>
-              <span>Altura:</span>
-              <span>${info.height }</span>
-            </li>
-            <li>
-              <span>Cor do cabelo:</span>
-              <span>${info.hair_color}</span>
-            </li>
-            <li>
-              <span>Ano de nascimento:</span>
-              <span>${ info.birth_year}</span>
-            </li>
-            <li>
-              <span>Naves:</span>
-              <span id="naves">
-                <ul></ul>
-              </span>
-            </li>
+            ${
+              info.starships.map(function(nave, i){
+                return `
+                  <li>
+                    <a href='${nave}'>
+                      Nave ${i + 1}
+                    </a>
+                  </li>
+                `
+              }).join('')
+            }
           </ul>
+        </span>
+      </li>
+    </ul>
   `
 
-
-
- // nome.innerHTML = info.name;
-  // altura.innerHTML = info.height + "cm";
-  // corCabelo.innerHTML = info.hair_color;
-  // anoNascimento.innerHTML = info.birth_year;
-
-  var paerfil = document.getElementById("perfil");
+  var perfil = document.getElementById("perfil");
   perfil.innerHTML = template;
 }
+
 
 // AJAX com jQuery
 $.ajax({
